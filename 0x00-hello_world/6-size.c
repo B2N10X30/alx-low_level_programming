7a321725ddcf4f8ecd5903976c85958bfdd53d93
B2N10X30 <samuelbonux10@gmail.com>
2021-08-30 00:42:22 +0100

    copmiling c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
- *main - print the argument in the printf function
- *Description: This program prints usin the main function
- *Return: 0
-**/
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints the size of the different types on a linux 64bit machine
+* Return: 0
+*/
 int main(void)
-
 {
-	char c;
-	int e;
-	long ie;
-	long long lie;
-	float fl;
-	printf("Size of a char: %lu bytes(s)\n", sizeof(c));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(e));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(ie));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(lie));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(fl));
-	return (0); 
+char c;
+int e;
+long ie;
+long long lie;
+float fl;
+
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(e));
+printf("Size of a long int: %ld byte(s)\n", sizeof(ie));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lie));
+printf("Size of a float: %ld byte(s)\n", sizeof(fl));
+return (0);
 }
