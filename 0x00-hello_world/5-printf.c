177531d5e3282d9f757e725bfbb1818669da36d5
B2N10X30 <samuelbonux10@gmail.com>
2021-08-29 10:49:54 +0100

    copmiling c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints the argument in the printf fucntion
+ *Description: this program prints the argument using the main function
+*Return: 0
+**/
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
