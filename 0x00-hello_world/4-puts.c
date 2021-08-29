fe609851cb58b11f09eb4aab2d24522de6b9dc14
B2N10X30 <samuelbonux10@gmail.com>
2021-08-29 10:31:20 +0100

    copmiling c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main -  print outputs the argument in the put fucntion
+ *Decription: this program prints what is declared by the main function
+ *"Programming is like building a multilingual puzzle
+ *Return: 0
+**/
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
