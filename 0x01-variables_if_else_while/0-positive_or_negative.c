5f2d41522bd5ed4ea9869e4837a2b8d33efd1964
B2N10X30 <samuelbonux10@gmail.com>
2021-08-30 08:11:19 +0100

    code on if statement in c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+include <stdlib.h>
+#include <time.h>
+
+/* This program detects if an integer n is +ve, -ve or is zero
+* by using the logic in the main function
+* main - print if n is +ve,zero or -ve
+Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else 
+	{
+		printf("%i is negative", n);
+	}
+	return (0);
+}
