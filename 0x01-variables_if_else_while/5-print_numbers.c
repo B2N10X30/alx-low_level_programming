f5164796ce54df78f1ba34050f00b7726eb92b09
B2N10X30 <samuelbonux10@gmail.com>
2021-08-30 22:14:11 +0100

    code on if statement in c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,13 @@
 */
 int main(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		printf("%i", i)
-	}
-	putchar('\n');
+int i;
 
-	return (0);
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+putchar('\n');
+
+return (0);
 }
