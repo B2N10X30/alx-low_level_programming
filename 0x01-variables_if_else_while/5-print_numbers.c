b0aaf47cc4f38c1255343c364ce98051c4594dd5
B2N10X30 <samuelbonux10@gmail.com>
2021-08-30 21:51:25 +0100

    code on if statement in c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main uses for loop to loop through a given set
+ * of object(arg)
+ * Description: print all numbers of base 10 which contains one digit
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+ 	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
