fc8bdb1ba288c5aa12d7e638628bb64739720c64
B2N10X30 <samuelbonux10@gmail.com>
2021-09-03 21:51:43 +0100

    code on if statement in c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
+include "main.h"
 #include <stdio.h>
-#include "main.h"
-
+/**
+* _isupper - function that checks for uppercase character.
+* @c: the int for the paramaters of my function
+* Return: 0
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
