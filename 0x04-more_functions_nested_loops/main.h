4d9934824843c7aecc65c0ca06f72f18d3ac9dce
B2N10X30 <samuelbonux10@gmail.com>
2021-09-03 21:55:16 +0100

    looping yeah?

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-int _isupper(char c);
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,5 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-
-
+#endif
