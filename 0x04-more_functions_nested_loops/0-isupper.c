8c39d52d2279fed29e5b2e42819065724e169f0a
B2N10X30 <samuelbonux10@gmail.com>
2021-09-03 22:51:10 +0100

    looping yeah?

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,8 @@
+#include "main.h"
 #include <stdio.h>
 /**
 * _isupper - function that checks for uppercase character.
+* @c: the int for the paramaters of my function
 * Return: 0
 */
 int _isupper(int c)
