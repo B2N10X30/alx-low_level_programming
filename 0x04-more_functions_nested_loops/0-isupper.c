124cf604e368f20edfa9f1f64e4c4ef16afa9ca9
B2N10X30 <samuelbonux10@gmail.com>
2021-09-03 21:16:55 +0100

    code on if statement in c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(void)
+int _isupper(int c)
 {
-	int _isupper(inc)
-	{
-		if (c >= 'A' && c <= 'Z')
-		{
-			return (1);
-		}
-		else
-			return (0);
-	}
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+return (0);
 }
